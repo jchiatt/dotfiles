ff85d86054ffd0c2b5afeb15647a08b53309eb10
J.C. Hiatt <jc@jchiatt.com>
2024-12-28 09:03:16 -0600

    install pyenv via https

--- setup.d/pyenv.sh
+++ setup.d/pyenv.sh
@@ -7,8 +7,8 @@ PATH="${virtualenv_path}/bin:$PATH"
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 eval "$(${language}env init -)"
 
-git clone git://github.com/pyenv/pyenv-update.git "$(pyenv root)/plugins/pyenv-update"
+git clone https://github.com/pyenv/pyenv-update.git "$(pyenv root)/plugins/pyenv-update"
 git clone https://github.com/jawshooah/pyenv-default-packages.git "$(pyenv root)/plugins/pyenv-default-packages)"
-echo "pip" >>"$(rbenv root)/default-packages"
+echo "pip" >>"$(pyenv root)/default-packages"
 
 exec "${SHELL}"
